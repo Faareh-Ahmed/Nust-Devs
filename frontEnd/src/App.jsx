d8991030f1cb4453dd216aa8e417b46b32164d6c
Badar Ur Zaman <hbrsadiqian123@gmail.com>
2024-02-24 12:25:51 +0500

    Nav structure

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Register from './Register'
 import Home from './Home'
+import Login from './Login'
 
 function App() {
 
--- frontEnd/src/Home.jsx
+++ frontEnd/src/Home.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 function Home(){
     return ( 
-        <div>Home</div>
+        <div className="nav">
+            <ul>
+                <li>Home</li>
+                <li>Umrah Hajj</li>
+                <li>Pakistan Tour</li>
+                <li>About Us</li>
+                <li>Sign In</li>
+            </ul>
+        </div>
      );
 }
 
