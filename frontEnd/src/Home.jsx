43eff46024ca608a639f701d7e67c150fa6b4fde
Hadiya-Ali <hadiapapa123@gmail.com>
2024-02-24 17:44:04 +0500

    Footer

--- frontEnd/src/Home.jsx
+++ frontEnd/src/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import './index.css';
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 import HomePg from "./components/home-pg";
 
 function Home() {
