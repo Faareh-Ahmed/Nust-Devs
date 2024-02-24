235eae21bb37159299cf3e014367d486b4e3a167
Badar Ur Zaman <hbrsadiqian123@gmail.com>
2024-02-24 23:55:20 +0500

    Login And Register Backend

---
+++ server/controller/auth.js
@@ -0,0 +1,63 @@
+import {db} from '../db.js';
+import bcrypt from 'bcrypt';
+
+export const register = (req, res) => {
+    // Check if user already exists
+
+    const q = "select * from Users where email = ?";
+
+    db.query(q, [req.body.email], (err, data) => {
+        if(err) return res.json(err)
+        if(data.length > 0) return res.status(409).json("User Already Exist")
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+        
+        const insertQuery = 'insert into Users(first_name, last_name, email, password) Values(?,?,?,?)';
+        const values = [req.body.first_name, req.body.last_name, req.body.email, hash];
+        
+        db.query(insertQuery, values, (err, data) => {
+            if(err) return res.json(err)
+            if(data){
+                return res.status(201).json("User has been created Successfully!");
+            }
+        })
+    });
+
+};
+
+export const login = (req, res) => {
+    //Check user exist or not
+    const q = 'select * from Users where email = ?'
+    db.query(q, [req.body.email], (err, data) => {
+        if(err) return res.json(err)
+        if(data.length === 0) return res.status(404).json("User not found!");
+
+        console.log(data);
+        //Check password
+        const isPasswordCorrect = bcrypt.compareSync(
+            req.body.password, 
+            data[0].password
+        );
+        
+        if(!isPasswordCorrect) return res.status(400).json("Wrong username or password!")
+        
+        // const token = jwt.sign({id: data[0].id}, "jwtkey");
+        // // const { password, ...hehe } = data[0];
+        
+        // res.cookie("access_token", token, {
+        //     httpOnly:true,
+        //     sameSite:'None',
+        //     secure:true
+        // }).status(200).json(data);
+    })
+}
+
+// export const logout = (req, res) => {
+//     res.clearCookie("access_token", {
+//         sameSite: "none", 
+//         secure: true
+//     })
+//     .status(200)
+//     .json("User has been logged out");
+// }
---
+++ server/routes/post.js
@@ -0,0 +1,21 @@
+// import express from "express";
+// import { addPost, updatePost, deletePost, getPost, getPosts } from "../controller/post.js";
+// import { getPost } from "../controller/post.js"
+
+// const router = express.Router()
+
+// router.get("/print", getPost);
+// router.post("/", addPost);
+// router.get("/", (req, res) => {
+//     return res.json("Running");
+// });
+// router.delete("/:id", deletePost);
+// router.put("/:id", updatePost);
+
+// export default router;
+
+const postRoutes = (req, res) => {
+    res.send("sdjkbvk");
+}
+
+export default postRoutes;
