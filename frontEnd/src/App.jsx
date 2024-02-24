9381acc98be6487b6ca941015b049e7ee5217454
Faareh-Ahmed <faarehahmed@gmail.com>
2024-02-24 08:33:26 +0500

    folders structure

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Register from './Register'
+import Register from './pages/Register'
 import Home from './Home'
 
 function App() {
