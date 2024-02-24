e8de722c8648631ba16ed39473745446abaae8e2
Hadiya-Ali <hadiapapa123@gmail.com>
2024-02-25 00:07:28 +0500

    a

--- frontEnd/src/components/home-pg.jsx
+++ frontEnd/src/components/home-pg.jsx
@@ -4,7 +4,7 @@ import Image1 from "../images/bg-destinations.jpg"
 function HomePg() {
     return (
         <div className="">
-            <div className="flex bg-white opacity-90 h-main w-full items-center justify-center font-extrabold text-2xl font-mono">
+            <div className="flex text-white h-main w-full items-center justify-center font-extrabold text-2xl font-mono">
                 Showing you Pakistan, One place at a Time
             </div>
         </div>
