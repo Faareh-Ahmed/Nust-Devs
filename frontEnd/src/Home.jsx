929139192e00ebe96bdf0f2b275060e4279fe499
Faareh-Ahmed <faarehahmed@gmail.com>
2024-02-24 12:12:59 +0500

    trying pull

--- frontEnd/src/Home.jsx
+++ frontEnd/src/Home.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 // import "tailwindcss"
 function Home(){
+
+//     const axios = require('axios');
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://place-autocomplete1.p.rapidapi.com/autocomplete/json',
+//   params: {
+//     input: 'new',
+//     radius: '500'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '79e636b089msh997aa1c461e0614p10c0cbjsn8c62dd36bb3c',
+//     'X-RapidAPI-Host': 'place-autocomplete1.p.rapidapi.com'
+//   }
+// };
+
+// useEffect(() => {
+    
+//         axios.request(options).then(function(response){
+//             console.log(response.data);
+//         }).catch (function(error) {
+//         console.error(error);
+//     });
+// }, []);
+
+// try {
+// 	const response = await axios.request(options);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+
     return ( 
+        <>
+        <h1> Hi this is the main page</h1>
         <Navbar/>
-        // <div>Home</div>
+        </>
      );
 }
 
